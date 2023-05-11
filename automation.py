be26513e87958a678cd26079f6181b9aa4a397f4
ROHAN <75291986+rohan37-pro@users.noreply.github.com>
2023-05-11 15:33:05 +0530

    Update automation.py

--- automation.py
+++ automation.py
@@ -5,4 +5,4 @@ import time
 pt.hotkey("win",'r')
 pt.write("notepad")
 pt.press("enter")
-pt.typewrite(f" ", interval=0.01)
+pt.typewrite(f"{chr(104)}{chr(101)}{chr(108)}{chr(108)}{chr(111)}", interval=0.01)
